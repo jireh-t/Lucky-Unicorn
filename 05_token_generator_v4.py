0462eb767089e1312514e9591bd7e76523bd51b2
jireh-t <98923573+jireh-t@users.noreply.github.com>
2022-04-13 19:30:24 +1200

    Generator

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -9,19 +9,25 @@ import random
 STARTING_BALANCE = 100
 balance = STARTING_BALANCE
 
-# Testing loop to generate 20 tokens
+# Testing loop to generate 100 tokens
 for item in range(10):
     number = random.randint(1, 100)
 
     # Adjust balance
-    # If the number is between 1 and 5
+    # If the random number is between 1 and 5
     # User gets a unicorn (add $4 to balance)
     if 1 <= number <= 5:
         token = "unicorn"
         balance += 4
+
+    # If the number is between 6 and 36
+    # User gets a donkey (subtract $1 from balance)
     elif 6 <= number <= 36:
         token = "donkey"
         balance -= 1
+
+    # In all other cases, the token must be a horse or zebra
+    # (subtract $0.50 from the balance in either case)
     else:
         # if the number is even, set the token to zebra
         if number % 2 == 0:
@@ -36,5 +42,6 @@ for item in range(10):
     print(f"Token: {token}, Balance: {balance:.2f}")
 
 print(f"Token: {token}, Balance: {balance:.2f}")
+print()
 print(f"Starting balance : ${STARTING_BALANCE:.2f}")
 print(f"Final balance: ${balance:.2f}")
